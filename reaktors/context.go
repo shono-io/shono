1433b80783d555966c1ad3a74e3eeb6de7344f4c
Daan Gerits <dgerits@korfinancial.com>
2023-04-24 20:50:23 +0200

    added the reaktors for concepts and events

--- reaktors/context.go
+++ reaktors/context.go
@@ -8,11 +8,20 @@ import (
 	"time"
 )
 
+type SendOpt func(r *kgo.Record)
+
+func WithKey(key string) SendOpt {
+	return func(r *kgo.Record) {
+		r.Key = []byte(key)
+	}
+}
+
 type ReaktorContext interface {
 	context.Context
+	Key() string
 	Timestamp() time.Time
 	Failed(kind events.Kind, err error)
-	Send(kind events.Kind, key string, value any)
+	Send(kind events.Kind, value any, opts ...SendOpt)
 	Header(key string) []byte
 }
 
@@ -21,6 +30,10 @@ type reaktorContext struct {
 	kc     *kgo.Client
 	er     events.EventRegistry
 	record *kgo.Record
+}
+
+func (ctx *reaktorContext) Key() string {
+	return string(ctx.record.Key)
 }
 
 func (ctx *reaktorContext) Timestamp() time.Time {
@@ -38,10 +51,10 @@ func (ctx *reaktorContext) Header(key string) []byte {
 }
 
 func (ctx *reaktorContext) Failed(kind events.Kind, err error) {
-	ctx.Send(kind, string(ctx.record.Key), events.OperationFailed{Reason: err.Error()})
+	ctx.Send(kind, events.OperationFailed{Reason: err.Error()})
 }
 
-func (ctx *reaktorContext) Send(kind events.Kind, key string, value any) {
+func (ctx *reaktorContext) Send(kind events.Kind, value any, opts ...SendOpt) {
 	ei, err := ctx.er.Event(ctx, kind)
 	if err != nil {
 		panic(fmt.Sprintf("error getting event info: %v", err))
@@ -53,12 +66,16 @@ func (ctx *reaktorContext) Send(kind events.Kind, key string, value any) {
 	}
 
 	record := &kgo.Record{
-		Key:   []byte(key),
+		Key:   ctx.record.Key,
 		Value: val,
 		Topic: kind.Domain,
 		Headers: []kgo.RecordHeader{
 			{Key: events.KindHeader, Value: []byte(kind.String())},
 		},
+	}
+
+	for _, opt := range opts {
+		opt(record)
 	}
 
 	if pr := ctx.kc.ProduceSync(ctx, record); pr.FirstErr() != nil {
